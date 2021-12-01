422a28f67e710b260471a957197802f9ee3f60d4
Evan McGrane <mcgrane480@gmail.com>
2021-12-01 19:29:54 +0000

    building login system

--- src/App.js
+++ src/App.js
@@ -10,18 +10,23 @@ import Home from "./components/Home"
 import Movie from "./components/Movie";
 import NotFound from "./components/NotFound";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserProvider from './context';
+import Login from './components/Login';
 
 
 const App = () => (
     <Router className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:movieId" element={<Movie/>} />
-        <Route path="/*" element={<NotFound/>} />
+      <UserProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />}/>
+          <Route path="/:movieId" element={<Movie/>} />
+          <Route path="/*" element={<NotFound/>} />
 
-      </Routes>
-      <GlobalStyle />
+        </Routes>
+        <GlobalStyle />
+      </UserProvider>
 
     </Router>
 );
